61a7a5599d7682d52ac51ba914b1d1f99f0b2ef9
BeatACVR <75857504+BeatACVR@users.noreply.github.com>
2025-10-21 19:43:11 -0500

    go

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+std::string floatToRoman(float num) {
+    std::string res;
+    int n = static_cast<int>(std::round(num));
+    if (n == 0) {
+        res = "0";
+        return res;
+    }
+    // i thought over whether id need this or not for a while but its better to be safe ig
+    if (n < 0 || n > 100) {
+        res.clear();
+        return res;
+    }
+    int vals[] = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+    std::string syms[] = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    res.clear();
+    for (int i = 0; i < 9; ++i) {
+        while (n >= vals[i]) {
+            n -= vals[i];
+            res += syms[i];
+        }
+    }
+    return res;
+}
+
+class $modify(rnPlayLayer, PlayLayer) {
+    void updateProgressbar() {
+        PlayLayer::updateProgressbar();
+        float ret = this->getCurrentPercent();
+        m_percentageLabel->setString((floatToRoman(ret) + std::string("%")).c_str());
+    }
+};
